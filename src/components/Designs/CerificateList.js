dc9e3ce55cde931576dd314c23ebc78345b9f522
pandaomprasad <omprasadpanda1@gmail.com>
2024-11-18 10:33:10 +0530

    Updated

--- src/components/Designs/CerificateList.js
+++ src/components/Designs/CerificateList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./CertificateList.css";
 
 import ISOBAJARANGi from "../../assets/ISOBAJARANGI.jpg";
 import IECBAJARANGI from "../../assets/IECBAJARANGI.jpg";
@@ -8,32 +9,32 @@ import UDYAMBAJARANGI from "../../assets/UDYAMBAJARANGI.jpg";
 const certificates = [
   {
     name: 'GST Certificate',
-    image:{GSTBAJARANGI}
+    image:GSTBAJARANGI
   },
   {
     name: 'IEC Certificate',
-    image: {IECBAJARANGI}
+    image: IECBAJARANGI
   },
   {
     name: 'ISO Certificate',
-    image: {ISOBAJARANGi}
+    image: ISOBAJARANGi
   },
   {
     name: 'UDYAM REGISTRATION Certificate',
-    image:{UDYAMBAJARANGI}
+    image:UDYAMBAJARANGI
   },
 ];
 
 const CertificateCard = ({ name, image }) => (
-  <div style={styles.card}>
-    <img src={image} alt={name} style={styles.image} />
-    <h3 style={styles.name}>{name}</h3>
+  <div className='certifiacte-card'>
+    <img src={image} alt={name} className='certifiacte-image' />
+    <h3 className='certifiacte-name'>{name}</h3>
   </div>
 );
 
 const CertificateList = () => {
   return (
-    <div style={styles.container}>
+    <div className='certifiacte-container'>
       {certificates.map((cert, index) => (
         <CertificateCard key={index} name={cert.name} image={cert.image} />
       ))}
@@ -41,37 +42,6 @@ const CertificateList = () => {
   );
 };
 
-const styles = {
-  container: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-    gap: '16px',
-    padding: '16px',
-  },
-  card: {
-    border: '1px solid #ddd',
-    borderRadius: '8px',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-    textAlign: 'center',
-    padding: '16px',
-    backgroundColor: '#fff',
-    transition: 'transform 0.2s ease',
-    cursor: 'pointer',
-  },
-  cardHover: {
-    transform: 'scale(1.05)',
-  },
-  image: {
-    width: '100%',
-    height: 'auto',
-    borderRadius: '8px',
-    marginBottom: '8px',
-  },
-  name: {
-    fontSize: '16px',
-    fontWeight: 'bold',
-    color: '#333',
-  },
-};
+
 
 export default CertificateList;
